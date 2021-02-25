2cfa1a523e33ff292c7d59d1741f92dbd059a860
Alexandre Marotti da fonseca Temporal <pvp_marotti@hotmail.com.br>
2021-02-25 06:29:08 -0300

    criação da estrutura básica responsável por realizar a conexão com o servidor

--- js/handleConnection.js
+++ js/handleConnection.js
@@ -1,6 +1,22 @@
-var temp = null;
-$(document).ready(function () {
-    
-    
+var server = new ServerHandler("service/")
+class ServerHandler{
+	constructor(url){
+		this.destination = url;
+	}
+	serverRequest(requestParams){
+		return new Promise((resolve, reject) =>{
+			$.ajax({
+				url: this.destination,
+				method: "POST",
+				data: JSON.stringify(requestParams),
+				dataType: 'json',
+				contentType: 'application/json'
+	
+	
+			}).done((data)=>{
+				resolve(data);
+			});
+		});
+	}
+}
 
-});
